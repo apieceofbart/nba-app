ed54042305ac6086b64ec332a62fc800b515b023
apieceofbart <bartek.kozera@gmail.com>
2019-03-09 13:17:08 +0100

    add fetching games from api and showing basic info

---
+++ src/backend/interfaces/nba.response.games.ts
@@ -0,0 +1,115 @@
+/* created using example data and https://transform.now.sh/json-to-ts-interface/ */
+export type GamesResponse = GameDetails[];
+export interface GameDetails {
+  id: string;
+  game_url: string;
+  season_id: string;
+  date: string;
+  time: string;
+  arena: string;
+  city: string;
+  state: string;
+  country: string;
+  home_start_date: string;
+  home_start_time: string;
+  visitor_start_date: string;
+  visitor_start_time: string;
+  previewAvailable: string;
+  recapAvailable: string;
+  notebookAvailable: string;
+  tnt_ot: string;
+  buzzerBeater: string;
+  ticket: Ticket;
+  period_time: Period_time;
+  lp: Lp;
+  dl: Dl;
+  broadcasters: Broadcasters;
+  visitor: Visitor;
+  home: Home;
+}
+interface Ticket {
+  ticket_link: string;
+}
+interface Period_time {
+  period_value: string;
+  period_status: string;
+  game_status: string;
+  game_clock: string;
+  total_periods: string;
+  period_name: string;
+}
+interface Lp {
+  lp_video: string;
+  condensed_bb: string;
+  visitor: Visitor;
+  home: Home;
+}
+interface Visitor {
+  audio?: Audio;
+  video?: Video;
+  id?: string;
+  team_key?: string;
+  city?: string;
+  abbreviation?: string;
+  nickname?: string;
+  url_name?: string;
+  team_code?: string;
+  score?: string;
+  linescores?: Linescores;
+}
+interface Audio {
+  ENG: string;
+  SPA: string;
+}
+interface Video {
+  avl: string;
+  onAir: string;
+  archBB: string;
+}
+interface Home {
+  audio?: Audio;
+  video?: Video;
+  id?: string;
+  team_key?: string;
+  city?: string;
+  abbreviation?: string;
+  nickname?: string;
+  url_name?: string;
+  team_code?: string;
+  score?: string;
+  linescores?: Linescores;
+}
+interface Dl {
+  link: LinkItem[];
+}
+interface Broadcasters {
+  radio: Radio;
+  tv: Tv;
+}
+interface Radio {
+  broadcaster: BroadcasterItem[];
+}
+interface BroadcasterItem {
+  scope: string;
+  home_visitor: string;
+  display_name: string;
+}
+interface Tv {
+  broadcaster: BroadcasterItem[];
+}
+interface Linescores {
+  period: PeriodItem[];
+}
+interface PeriodItem {
+  period_value: string;
+  period_name: string;
+  score: string;
+}
+interface LinkItem {
+  name: string;
+  long_nm: string;
+  code: string;
+  url: string;
+  mobile_url: string;
+  home_visitor: string;
+}
--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -1,17 +1,31 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { GameDetails } from "../../backend/interfaces/nba.response.games";
 
-type AppProps = {
-  greetings?: string;
-};
-
-class App extends React.Component<AppProps> {
-  static defaultProps = {
-    greetings: "Hello there!"
-  };
-  render() {
-    return <h1>{this.props.greetings}</h1>;
-  }
+async function fetchGames(setGamesCallback: React.Dispatch<React.SetStateAction<GameDetails[]>>) {
+  const gamesResponse: GameDetails[] = await fetch("/api/").then((r) => r.json());
+  setGamesCallback(gamesResponse);
 }
 
-ReactDOM.render(<App greetings="Hello Typescript and React!" />, document.getElementById("root"));
+function App() {
+  const [games, setGames] = React.useState<GameDetails[]>([]);
+
+  React.useEffect(() => {
+    fetchGames(setGames);
+  }, []);
+
+  return (
+    <>
+      <h1>NBA App</h1>
+      <ul>
+        {games.map((game) => (
+          <li>
+            {game.home.nickname} - {game.visitor.nickname}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
