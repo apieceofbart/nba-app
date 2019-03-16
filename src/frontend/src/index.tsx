ff0bd4d9ae9f2f0c2caf0fa31da209ec03d4f2cc
apieceofbart <bartek.kozera@gmail.com>
2019-03-16 12:44:57 +0100

    add loading spinner

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -4,13 +4,17 @@ import { GameDetails } from "../../backend/interfaces/nba.response.games";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { Typography } from "@material-ui/core";
 
 function App() {
   const [games, setGames] = React.useState<GameDetails[]>([]);
+  const [loadingGames, setLoadingGames] = React.useState(false);
 
   async function fetchGames() {
+    setLoadingGames(true);
     const gamesResponse: GameDetails[] = await fetch("/api/").then((r) => r.json());
+    setLoadingGames(false);
     setGames(gamesResponse);
   }
 
@@ -19,23 +23,29 @@ function App() {
   }, []);
 
   return (
-    <>
-      <Typography variant="h1" style={{ textAlign: "center" }}>
-        NBA App
-      </Typography>
-      <List component="ul">
-        {games.map((game) => (
-          <ListItem key={game.id}>
-            <ListItemText
-              primary={game.home.nickname}
-              secondary={game.home.score}
-              style={{ textAlign: "right", flex: "1 1 50%" }}
-            />
-            <ListItemText primary={game.visitor.nickname} secondary={game.visitor.score} style={{ flex: "1 1 50%" }} />
-          </ListItem>
-        ))}
-      </List>
-    </>
+    <section style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
+      <Typography variant="h1">NBA App</Typography>
+      {loadingGames ? (
+        <CircularProgress />
+      ) : (
+        <List component="ul">
+          {games.map((game) => (
+            <ListItem key={game.id}>
+              <ListItemText
+                primary={game.home.nickname}
+                secondary={game.home.score}
+                style={{ textAlign: "right", flex: "1 1 50%" }}
+              />
+              <ListItemText
+                primary={game.visitor.nickname}
+                secondary={game.visitor.score}
+                style={{ flex: "1 1 50%" }}
+              />
+            </ListItem>
+          ))}
+        </List>
+      )}
+    </section>
   );
 }
 
