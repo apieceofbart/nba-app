49ed23c3b26441748e20b527eef02656e6cdfcfc
apieceofbart <bartek.kozera@gmail.com>
2019-03-17 22:15:59 +0100

    add sticky first column

--- src/frontend/src/GameDetails.tsx
+++ src/frontend/src/GameDetails.tsx
@@ -8,15 +8,23 @@ import TableRow from "@material-ui/core/TableRow";
 import { GameData } from "../../backend/interfaces/nba.response.games";
 import { Typography } from "@material-ui/core";
 
+const firstCellStyle = {
+  left: 0,
+  position: "sticky" as "sticky",
+  background: "#dedede"
+};
+
 export function GameDetails({ home, visitor }: { home: GameData["home"]; visitor: GameData["visitor"] }) {
   return (
-    <div style={{ display: "flex", flexDirection: "column", padding: "16px" }}>
+    <div style={{ display: "flex", flexDirection: "column", padding: "16px", position: "relative" }}>
       <Typography variant="subtitle2">Game Details</Typography>
       <div style={{ overflowX: "auto", maxWidth: "80vw" }}>
         <Table padding="dense">
           <TableHead>
             <TableRow>
-              <TableCell key="team">Team</TableCell>
+              <TableCell key="team" style={firstCellStyle}>
+                Team
+              </TableCell>
               {home.linescores!.period.map((p) => (
                 <TableCell key={p.period_name}>{p.period_name}</TableCell>
               ))}
@@ -25,14 +33,18 @@ export function GameDetails({ home, visitor }: { home: GameData["home"]; visitor
           </TableHead>
           <TableBody>
             <TableRow>
-              <TableCell key="team">{home.nickname}</TableCell>
+              <TableCell key="team" style={firstCellStyle}>
+                {home.nickname}
+              </TableCell>
               {home.linescores!.period.map((p) => (
                 <TableCell key={p.period_name}>{p.score}</TableCell>
               ))}
               <TableCell key="score">{home.score}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell key="team">{visitor.nickname}</TableCell>
+              <TableCell key="team" style={firstCellStyle}>
+                {visitor.nickname}
+              </TableCell>
               {visitor.linescores!.period.map((p) => (
                 <TableCell key={p.period_name}>{p.score}</TableCell>
               ))}
