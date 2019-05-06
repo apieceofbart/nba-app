1f12ad0e82c893da8eb69a0c22063ce569c29de1
apieceofbart <bartek.kozera@gmail.com>
2019-05-06 21:58:00 +0200

    add space

--- src/frontend/src/PointsTable.tsx
+++ src/frontend/src/PointsTable.tsx
@@ -67,7 +67,7 @@ export function PointsTable({ data }: { data: BoxScore["home"] }) {
             <TableCell key="drebounds">{data.stats!.rebounds_defensive}</TableCell>
             <TableCell key="orebounds">{data.stats!.rebounds_offensive}</TableCell>
             <TableCell key="field_goals">
-              {data.stats!.field_goals_made} / {data.stats!.field_goals_attempted}({data.stats!.field_goals_percentage}
+              {data.stats!.field_goals_made} / {data.stats!.field_goals_attempted} ({data.stats!.field_goals_percentage}
               %)
             </TableCell>
             <TableCell key="threes">
